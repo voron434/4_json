1b7349cc1c2bfa8c274d889e7b39bb77623356d0
voron434 <070809010@mail.ru>
2017-04-07 09:44:55 +0300

    added solution

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,19 @@ import json
 
 
 def load_data(filepath):
-    pass
-
+    with open(filepath, mode='r', encoding='utf-8') as my_file:
+        data = json.load(my_file)
+        return data
 
 def pretty_print_json(data):
-    pass
-
+    print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
 
 if __name__ == '__main__':
-    pass
+    print('Enter filepath to your database:')
+    path = input()
+    try:
+        data = load_data(path)
+    except FileNotFoundError:
+        print('File not found, sorry...')
+        raise SystemExit
+    pretty_print_json(data)
